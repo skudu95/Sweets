4489e8834bf31ec2657bf1c9514d7cfe657e44d5
Sharjeel Karim <44142391+skudu95@users.noreply.github.com>
2022-05-29 15:56:06 +0600

    Initial commit

---
+++ app/src/main/java/com/kudu/sweets/activities/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.kudu.sweets.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.kudu.sweets.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnInstagram.setOnClickListener {
+            Toast.makeText(this, "Instagram clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com")))
+        }
+        binding.btnFacebook.setOnClickListener {
+            Toast.makeText(this, "Facebook clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com")))
+        }
+        binding.btnTwitter.setOnClickListener {
+            Toast.makeText(this, "Twitter clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.twitter.com")))
+        }
+    }
+
+
+}
